84926c08b22a818b761c9515e7f88b616e9ef569
ivan.virchenko <virchenkoivanmail@gmail.com>
2024-07-21 15:59:17 +0300

    additional source folder for cit

---
+++ src/cit/java/com/gradle/it_source/ItSourceApplicationIntTest.java
@@ -0,0 +1,10 @@
+package com.gradle.it_source;
+
+import org.junit.jupiter.api.Test;
+
+public class ItSourceApplicationIntTest {
+    @Test
+    void integrationTest() {
+        System.out.println("Hello from integration test");
+    }
+}
---
+++ src/test/java/com/gradle/it_source/ItSourceApplicationTest.java
@@ -0,0 +1,12 @@
+package com.gradle.it_source;
+
+import org.junit.jupiter.api.Test;
+
+class ItSourceApplicationTest {
+
+    @Test
+    void unitTest() {
+        System.out.println("Hello from unit test");
+    }
+
+}
