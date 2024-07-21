2abfbe8f71524ce87b31208277b517caf97f5d7b
ivan.virchenko <virchenkoivanmail@gmail.com>
2024-07-21 15:47:00 +0300

    repo init commit

---
+++ src/main/java/com/gradle/it_source/ItSourceApplication.java
@@ -0,0 +1,13 @@
+package com.gradle.it_source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItSourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gradle/it_source/ItSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradle.it_source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
